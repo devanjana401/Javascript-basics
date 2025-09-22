84a312a47efccc8cba4160c615d8135f6319c9b3
devanjana401 <anjanasasiaduvad@gmail.com>
2025-09-22 21:52:52 +0530

    Added new files

--- workout/for-loop/pyramid.js
+++ workout/for-loop/pyramid.js
@@ -1,13 +1,13 @@
 let n = 5;
 for (let i = 1; i <= n; i++) {
     let row = "";
-    for (let j = i; j < n; j++) {                // first loop for spaces
+    for (let j = i; j < n; j++) {                
         row += " ";
     }
-    for (let k = 1; k <= (2 * i - 1); k++) {      // second loop for stars on left
+    for (let k = 1; k <= (2 * i - 1); k++) {      
         row += "";
     }
-    for (let k = 1; k <= (2 * i - 1); k++) {      // second loop for stars on left
+    for (let k = 1; k <= (2 * i - 1); k++) {      
         row += "*";
     }
     console.log(row);
