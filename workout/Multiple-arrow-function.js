99fd060cb8e5cb33bf25fa312e5e4e8331efb9d5
devanjana401 <anjanasasiaduvad@gmail.com>
2025-09-24 21:47:38 +0530

    Added new files

---
+++ workout/Multiple-arrow-function.js
@@ -0,0 +1,21 @@
+const calc = (a, b) => ({
+    multiply: a * b,
+    add: a + b,
+    subtract: a - b,
+    divide: a / b
+});
+console.log(calc(2,5));
+
+
+
+
+// const calc = (a, b) => {
+//   return {
+//     multiply: a * b,
+//     add: a + b,
+//     subtract: a - b,
+//     divide: a / b
+//   };
+// };
+// console.log(calc(2,5));
+
